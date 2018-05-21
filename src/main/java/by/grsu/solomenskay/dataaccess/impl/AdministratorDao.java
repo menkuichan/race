8e1f7f888dba3cef7c254d8f1dbbf03cb74cc29a
MenkuiChan <ilonaspolding@gmail.com>
2018-05-21 14:45:10 +0300

    generify update method

--- src/main/java/by/grsu/solomenskay/dataaccess/impl/AdministratorDao.java
+++ src/main/java/by/grsu/solomenskay/dataaccess/impl/AdministratorDao.java
@@ -3,9 +3,6 @@ package by.grsu.solomenskay.dataaccess.impl;
 import by.grsu.solomenskay.datamodel.Administrator;
 import by.grsu.solomenskay.tables.AdministratorTable;
 
-import java.util.List;
-import java.util.Optional;
-
 public class AdministratorDao extends AbstractDao<AdministratorTable, Administrator> {
 
     public AdministratorDao(String rootFolderPath) {
@@ -15,25 +12,5 @@ public class AdministratorDao extends AbstractDao<AdministratorTable, Administra
     @Override
     protected Class<AdministratorTable> getTableClass() {
         return AdministratorTable.class;
-    }
-
-    @Override
-    public Administrator update(Administrator entity) {
-        // get existing data
-        final AdministratorTable table = deserializeFromXml();
-        // find by ID
-        for (final Administrator row : table.getRows()) {
-            if (row.getId().equals(entity.getId())) {
-                // found!!!
-                // copy data
-                row.setWorkNumber(entity.getWorkNumber());
-                row.setAge(entity.getAge());
-                row.setName(entity.getName());
-                row.setSurname(entity.getSurname());
-            }
-        }
-        // save updated table
-        serializeToXml(table);
-        return entity;
     }
 }
--- src/main/java/by/grsu/solomenskay/dataaccess/impl/BookmakerDao.java
+++ src/main/java/by/grsu/solomenskay/dataaccess/impl/BookmakerDao.java
@@ -1,7 +1,6 @@
 package by.grsu.solomenskay.dataaccess.impl;
 
 import by.grsu.solomenskay.datamodel.Bookmaker;
-import by.grsu.solomenskay.tables.AdministratorTable;
 import by.grsu.solomenskay.tables.BookmakerTable;
 
 public class BookmakerDao extends AbstractDao<BookmakerTable, Bookmaker> {
@@ -13,27 +12,5 @@ public class BookmakerDao extends AbstractDao<BookmakerTable, Bookmaker> {
     @Override
     protected Class<BookmakerTable> getTableClass() {
         return BookmakerTable.class;
-    }
-
-    @Override
-    public Bookmaker update(Bookmaker entity) {
-        // get existing data
-        final BookmakerTable table = deserializeFromXml();
-        // find by ID
-        for (final Bookmaker row : table.getRows()) {
-            if (row.getId().equals(entity.getId())) {
-                // found!!!
-                // copy data
-                row.setWorkNumber(entity.getWorkNumber());
-                row.setAge(entity.getAge());
-                row.setName(entity.getName());
-                row.setSurname(entity.getSurname());
-                row.setWorkAddress(entity.getWorkAddress());
-                row.setWorkExperience(entity.getWorkExperience());
-            }
-        }
-        // save updated table
-        serializeToXml(table);
-        return entity;
     }
 }
